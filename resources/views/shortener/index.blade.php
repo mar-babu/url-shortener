a4f21800a6ffefd9d6a1695bd8158be25dbbfe56
mar-babu <ar_cse@yahoo.com>
2024-09-27 22:25:44 +0600

    feat: short url list page design

---
+++ resources/views/shortener/index.blade.php
@@ -0,0 +1,65 @@
+<div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+    <div class="pb-4 bg-white dark:bg-gray-900">
+        <div class="grid w-full grid-cols-2 mt-4 mb-4">
+            <div class="inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none gap-2">
+                <svg class="w-4 h-4 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                     fill="none" viewBox="0 0 20 20">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                          d="M7.75 4H19M7.75 4a2.25 2.25 0 0 1-4.5 0m4.5 0a2.25 2.25 0 0 0-4.5 0M1 4h2.25m13.5 6H19m-2.25 0a2.25 2.25 0 0 1-4.5 0m4.5 0a2.25 2.25 0 0 0-4.5 0M1 10h11.25m-4.5 6H19M7.75 16a2.25 2.25 0 0 1-4.5 0m4.5 0a2.25 2.25 0 0 0-4.5 0M1 16h2.25"/>
+                </svg>
+                <h4 class="text-white">All Url List</h4>
+            </div>
+            <div class="flex justify-end">
+                <x-secondary-button class="flex gap-2">
+                    <svg class="w-4 h-4 text-gray-800 dark:text-white" aria-hidden="true"
+                         xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 18">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                              d="m1 14 3-3m-3 3 3 3m-3-3h16v-3m2-7-3 3m3-3-3-3m3 3H3v3"/>
+                    </svg>
+                    Short Url
+                </x-secondary-button>
+            </div>
+        </div>
+    </div>
+    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <tr>
+            <th scope="col" class="px-6 py-3">
+                Url name
+            </th>
+            <th scope="col" class="px-6 py-3">
+                Short url
+            </th>
+            <th scope="col" class="px-6 py-3">
+                Long Url
+            </th>
+            <th scope="col" class="px-6 py-3">
+                Clicks
+            </th>
+            <th scope="col" class="px-6 py-3">
+                Action
+            </th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                Md. Abdur Rahman's LinkedIn profile
+            </th>
+            <td class="px-6 py-4">
+                https://www.linkedin.com/in/md-abdur-rahman-368330139
+            </td>
+            <td class="px-6 py-4">
+                https://qaFxs4dFRecGjol
+            </td>
+            <td class="px-6 py-4">
+                22
+            </td>
+            <td class="px-6 py-4">
+                <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+            </td>
+        </tr>
+
+        </tbody>
+    </table>
+</div>
